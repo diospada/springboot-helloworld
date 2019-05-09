08bed8cc8f3817d69f6f219453d4669a9e2c4109
alberto bobice <abobice@sorint.it>
2019-05-09 16:10:56 +0200

    second attempt to copy external files on git using jenkins

--- src/main/resources/windows/deploy_service.ps1
+++
@@ -1,9 +0,0 @@
-param([String]$serviceName,[String]$jarName)
-$serviceNameXml = "$serviceName.xml"
-$serviceNameExe = "$serviceName.exe"
-
-write-output "Installing windows service $serviceName"
-#create xml file necessary to install microservice as windows service
-powershell -Command "(gc deploy_service.xml) -replace '%SERVICE_NAME%', '$serviceName' -replace '%JAR_NAME%', '$jarName' | Out-File -encoding ASCII $serviceNameXml"
-#rename exe that need to invoked to install microservice as windows service
-Rename-Item -Path 'WinSW.NET4.exe' -NewName $serviceNameExe
