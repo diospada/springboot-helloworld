3b66948db9affab778afe3a598079c94bef8a0cb
Alberto Bobice <alberto.bobice@gmail.com>
2019-05-20 16:25:34 +0200

    Update HomeControllerTest.java

--- src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
+++ src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
@@ -28,7 +28,7 @@ public class HomeControllerTest {
     public void helloWorld() {
         getRequest("/")
             .assertStatusCode(OK)
-            .assertResponseBody("Hello from GazGeek (PROVA 7)!");
+            .assertResponseBody("Hello from GazGeek (PROVA 8)!");
     }
 
     private HelloWorldResponse getRequest(String uri) {
