574177129d0c8d8f50093de1442916cc4220b03b
Alberto Bobice <alberto.bobice@gmail.com>
2019-05-20 15:02:06 +0200

    Update HomeController.java

--- src/main/java/com/gazgeek/helloworld/controller/HomeController.java
+++ src/main/java/com/gazgeek/helloworld/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @RequestMapping("/")
     String home() {
-        return "Hello from GazGeek (version 4)!";
+        return "Hello from GazGeek (version 5)!";
     }
 
 }
